fe0deca6454183b5f1b4392a0e7248437d0bf344
sanju-1105 <sanjusinghaniya23@gmail.com>
2026-05-11 22:42:28 +0530

    first website

---
+++ first python.py
@@ -0,0 +1,39 @@
+"""print("Hello world")
+print("my name is sanju")
+print("hello world.","my name is sanju")
+print(45)
+print(11+25)
+name = "sanju"
+age = 22
+height = 5.8
+print(height)                           Ctrl+Forward/
+print(name)
+print("my name is ", name)
+print("my age is", age) """
+"""
+a = 25
+b = 65
+sum = a + b
+print(sum) """
+
+# # Arithmetic operator = + - / * % **
+# a = 89
+# b = 78
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a%b) #reminder operator
+# print(a**b) #power operator
+
+# relational operator
+
+a = 85
+b = 56
+
+print(a == b)
+print(a != b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
